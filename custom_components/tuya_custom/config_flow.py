438275b61b6ef8d5eb60547e88bd7263e6ebfa7c
tanawoot <tanawootsingtongprasert@gmail.com>
2026-09-12 15:41:44 +0700

    save

--- custom_components/tuya_custom/config_flow.py
+++ custom_components/tuya_custom/config_flow.py
@@ -1,11 +1,10 @@
-"""Config flow for Tuya Custom integration."""
+"""Config flow for Tuya Cloud Door Lock integration."""
+import logging
 import voluptuous as vol
+
 from homeassistant import config_entries
-from homeassistant.helpers.selector import (
-    SelectSelector,
-    SelectSelectorConfig,
-    SelectSelectorMode,
-)
+from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
 
 from .const import (
     DOMAIN,
@@ -16,31 +15,11 @@ from .const import (
     DEFAULT_ENDPOINT,
 )
 
-ENDPOINTS = [
-    {"label": "Western America (us)", "value": "https://openapi.tuyaus.com"},
-    {"label": "Eastern America (ue)", "value": "https://openapi-ueaz.tuyaus.com"},
-    {"label": "Central Europe (eu)", "value": "https://openapi.tuyaeu.com"},
-    {"label": "Western Europe (we)", "value": "https://openapi-weaz.tuyaeu.com"},
-    {"label": "China (cn)", "value": "https://openapi.tuyacn.com"},
-]
-
-DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_CLIENT_ID): str,
-        vol.Required(CONF_CLIENT_SECRET): str,
-        vol.Required(CONF_ENDPOINT, default=DEFAULT_ENDPOINT): SelectSelector(
-            SelectSelectorConfig(
-                options=ENDPOINTS,
-                mode=SelectSelectorMode.DROPDOWN,
-            )
-        ),
-        vol.Required(CONF_DEVICE_ID): str,
-    }
-)
+_LOGGER = logging.getLogger(__name__)
 
 
-class TuyaCustomConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Tuya Custom."""
+class TuyaDoorLockConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Tuya Cloud Door Lock."""
 
     VERSION = 1
 
@@ -49,18 +28,24 @@ class TuyaCustomConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
+            # Set unique ID based on Device ID
             await self.async_set_unique_id(user_input[CONF_DEVICE_ID])
             self._abort_if_unique_id_configured()
 
             return self.async_create_entry(
-                title=f"Tuya Lock ({user_input[CONF_DEVICE_ID][-4:]})",
+                title=f"Door Lock ({user_input[CONF_DEVICE_ID][:6]}...)",
                 data=user_input,
             )
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=DATA_SCHEMA,
-            errors=errors,
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_CLIENT_ID): str,
+                vol.Required(CONF_CLIENT_SECRET): str,
+                vol.Required(CONF_DEVICE_ID): str,
+                vol.Optional(CONF_ENDPOINT, default=DEFAULT_ENDPOINT): str,
+            }
         )
 
-
+        return self.async_show_form(
+            step_id="user", data_schema=data_schema, errors=errors
+        )
