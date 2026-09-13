ceb528cdd84a90b90cea1d03485ad1b821495c6c
tanawoot <tanawootsingtongprasert@gmail.com>
2026-09-13 22:55:48 +0700

    add-motion

--- custom_components/tuya_custom/quirks/motion.py
+++ custom_components/tuya_custom/quirks/motion.py
@@ -1,8 +1,11 @@
-"""Tuya TS0601 Motion Sensor (_TZE204_b8vxct9l) Custom Quirk for ZHA (HA Core 2026+)."""
+"""Tuya TS0601 Motion Sensor (_TZE204_b8vxct9l) Custom Quirk for ZHA."""
 import math
+from typing import Dict
 
-from zhaquirks.tuya import TuyaQuirkBuilder
+from zigpy.profiles import zha
+from zigpy.quirks import CustomDevice
 from zhaquirks.tuya.mcu import TuyaMCUCluster
+from zigpy.zcl.clusters.general import Basic, Groups, Ota, Scenes, Time
 from zigpy.zcl.clusters.measurement import IlluminanceMeasurement
 from zigpy.zcl.clusters.security import IasZone
 
@@ -17,7 +20,7 @@ def illuminance_converter(lux_val: int) -> int:
 class TuyaMotionCluster(TuyaMCUCluster):
     """Custom Tuya MCU cluster for Motion Sensor DP mapping."""
 
-    dp_to_attribute = {
+    dp_to_attribute: Dict[int, TuyaMCUCluster.TuyaDPToAttributeMapping] = {
         # DP 1: Motion State (0 = Clear, 1 = Detected)
         1: TuyaMCUCluster.TuyaDPToAttributeMapping(
             IasZone.attributes_by_name["zone_status"].id,
@@ -35,21 +38,42 @@ class TuyaMotionCluster(TuyaMCUCluster):
     }
 
 
-(
-    TuyaQuirkBuilder("_TZE204_b8vxct9l", "TS0601")
-    .tuya_dp(
-        dp_id=1,
-        ep_attribute=IasZone.ep_attribute,
-        attribute_name="zone_status",
-        converter=lambda x: IasZone.ZoneStatus.Alarm_1 if x else 0,
-    )
-    .tuya_dp(
-        dp_id=12,
-        ep_attribute=IlluminanceMeasurement.ep_attribute,
-        attribute_name="measured_value",
-        converter=illuminance_converter,
-    )
-    .add_to_registry()
-)
+class TuyaMotionSensorB8vxct9l(CustomDevice):
+    """Tuya TS0601 Motion Sensor _TZE204_b8vxct9l."""
 
-# update hass 2026
+    signature = {
+        "models_info": [("_TZE204_b8vxct9l", "TS0601")],
+        "endpoints": {
+            1: {
+                "profile_id": zha.PROFILE_ID,
+                "device_type": zha.DeviceType.IAS_ZONE,
+                "input_clusters": [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                ],
+                "output_clusters": [Time.cluster_id, Ota.cluster_id],
+            }
+        },
+    }
+
+    replacement = {
+        "endpoints": {
+            1: {
+                "profile_id": zha.PROFILE_ID,
+                "device_type": zha.DeviceType.IAS_ZONE,
+                "input_clusters": [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    IasZone.cluster_id,
+                    IlluminanceMeasurement.cluster_id,
+                    TuyaMotionCluster,
+                ],
+                "output_clusters": [Time.cluster_id, Ota.cluster_id],
+            }
+        },
+    }
+
+
+# update
