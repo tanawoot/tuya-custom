432d232e7faff97d9b4a10f6271f5bd9aee516fe
tanawoot <tanawootsingtongprasert@gmail.com>
2026-09-14 10:30:28 +0700

    update-new-code

--- custom_components/tuya_custom/quirks/motion.py
+++ custom_components/tuya_custom/quirks/motion.py
@@ -1,5 +1,4 @@
 """Tuya mmWave Radar Custom Quirk for ZHA."""
-import math
 from typing import Dict
 
 from zigpy.profiles import zgp, zha
@@ -193,8 +192,7 @@ class TuyaMmwRadarCluster(NoManufacturerCluster, TuyaMCUCluster):
         9: DPToAttributeMapping(
             TuyaMmwRadarTargetDistance.ep_attribute,
             "present_value",
-            converter=lambda x: float(x) / 100.0,
-            endpoint_id=6,
+            converter=lambda x: float(x) / 100.0 if x is not None else 0.0,
         ),
         101: DPToAttributeMapping(
             TuyaMmwRadarDetectionDelay.ep_attribute,
@@ -213,7 +211,7 @@ class TuyaMmwRadarCluster(NoManufacturerCluster, TuyaMCUCluster):
         103: DPToAttributeMapping(
             TuyaIlluminanceMeasurement.ep_attribute,
             "measured_value",
-            converter=lambda x: int(10000 * math.log10(x) + 1) if x and x > 1 else (0 if x == 0 else 1),
+            converter=lambda x: int(x) if x is not None else 0,
         ),
     }
 
@@ -268,6 +266,7 @@ class TuyaMmwRadarOccupancy(CustomDevice):
                     TuyaMmwRadarCluster,
                     TuyaIlluminanceMeasurement,
                     TuyaOccupancySensing,
+                    TuyaMmwRadarTargetDistance,
                     TuyaMmwRadarSensitivity,
                 ],
                 OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
@@ -304,15 +303,5 @@ class TuyaMmwRadarOccupancy(CustomDevice):
                 ],
                 OUTPUT_CLUSTERS: [],
             },
-            6: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.COMBINED_INTERFACE,
-                INPUT_CLUSTERS: [
-                    TuyaMmwRadarTargetDistance,
-                ],
-                OUTPUT_CLUSTERS: [],
-            },
         }
     }
-
-# update
